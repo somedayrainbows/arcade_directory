709573b27c8608d3410b51a0bf99406da21071d6
Erin Bassity <erin.bassity@cognizant.com>
2019-06-26 08:48:00 -0600

    Add routes for findOne and create

--- routes/api/v1/games.js
+++ routes/api/v1/games.js
@@ -4,13 +4,46 @@ const Game = require('../../../models').Game
 
 router.get('/', function(req, res, next) {
   Game.findAll()
-  .then(games => {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(200).send(JSON.stringify(games))
+    .then(games => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).send(JSON.stringify(games))
+    })
+    .catch(error => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(500).send({ error })
+    })
+})
+
+router.get('/:id', function(req, res, next) {
+  Game.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(game => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(200).send(JSON.stringify(game))
+    })
+    .catch(error => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(500).send({ error })
+    })
+})
+
+router.post('/', function(req, res, next) {
+  Game.create({
+    title: req.body.title,
+    price: req.body.price,
+    releaseYear: req.body.releaseYear,
+    active: req.body.active
+  })
+  .then(game => {
+    res.setHeader('Content-Tpye', 'application/json')
+    res.status(201).send(JSON.stringify(game))
   })
   .catch(error => {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(500).send({error})
+    res.setHeader('Content-type', 'application/json')
+    res.status(500).send({ error })
   })
 })
 
