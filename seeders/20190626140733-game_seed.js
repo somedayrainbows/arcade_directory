d579d81f72a5d4a53ee2054cda62735cf279f833
Erin Bassity <erin.bassity@cognizant.com>
2019-06-26 08:13:00 -0600

    Add seeds

---
+++ seeders/20190626140733-game_seed.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Games', [{
+      title: 'Ms. Pac Man',
+      price: 100,
+      releaseYear: 1981,
+      active: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Diner Dash',
+      price: 50,
+      releaseYear: 2002,
+      active: true,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Space Invaders',
+      price: 75,
+      releaseYear: 1988,
+      active: false,
+      createdAt: new Date(),
+      updatedAt: new Date()      
+    }])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Games', null, {})
+  }
+};
